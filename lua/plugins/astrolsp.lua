fdb80b630b5e2a53371e126ecd1ce93f49582bd4
Leonid Danilov <leoniddanilov@storyteq.com>
2024-09-11 13:22:01 +0200

    wip

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -29,8 +29,9 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
-        "tsserver",
+        "ts_ls",
         "eslint",
+        "volar",
       },
       timeout_ms = 5000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
