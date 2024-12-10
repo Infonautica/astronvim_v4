5bfefbb74cbb7d80b87b795d44514ad4f49e34ba
Leonid Danilov <leoniddanilov@storyteq.com>
2024-12-10 14:25:50 +0100

    Disabled markdown formatting

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -25,6 +25,7 @@ return {
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
           "yaml",
+          "markdown",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
